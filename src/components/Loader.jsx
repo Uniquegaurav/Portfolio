2e0d020426f2b1ecbab50c47665a4a993b1e74f6
gaurav <k.gauravsingh.9234.gs@gmail.com>
2024-09-24 01:58:41 +0530

    keeping loader same

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,19 +1,13 @@
 import { Html } from "@react-three/drei";
-import Loader from 'react-loaders';
-import 'loaders.css/src/animations/ball-scale-ripple-multiple.scss'; 
 
-const CustomLoader = ({ loading }) => {
+const Loader = () => {
   return (
     <Html center>
       <div className='flex items-center justify-center w-full h-full'>
-        {loading ? (
-          <Loader type="ball-scale-ripple-multiple" />
-        ) : (
-          <div className='w-20 h-20 border-4 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'></div>
-        )}
+        <div className='w-20 h-20 border-4 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'></div>
       </div>
     </Html>
   );
 };
 
-export default CustomLoader;
+export default Loader;
